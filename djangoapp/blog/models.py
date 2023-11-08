189130eab690851fafc8ecf253a67edda2e8f357
CidineiPuto <rafaelalvesqueiroz02@gmail.com>
2023-11-08 18:06:18 -0300

    Criando o model post

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from utils.rands import slugify_new
 
@@ -80,3 +81,62 @@ class Page(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        unique=True,
+        default='',
+        null=False,
+        blank=True,
+        max_length=65,
+    )
+    excerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisará estar marcado para a página ser exibida '
+            'publicamente.'
+        ),
+    )
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m/', blank=True, default='')
+    cover_in_post_content = models.BooleanField(
+        default=True,
+        help_text='Exiba a imagem de capa também dentro do conteúdo do post, se marcado.',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='post_created_by'
+    )
+    updated_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='post_updated_by'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
+    )
+    tag = models.ManyToManyField(
+        Tag, blank=True, default=''
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+
+        return super().save(*args, **kwargs)
